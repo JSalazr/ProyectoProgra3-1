8aec6f2938d9656e0c55626eb632a9e7d92689a2
Segebre <Juansegebre@unitec.edu>
2015-09-27 16:38:23 -0600

    collisions con tipo

---
+++ obstaculos.h
@@ -0,0 +1,25 @@
+#ifndef OBSTACULOS_H
+#define OBSTACULOS_H
+#include <iostream>
+#include <allegro5/allegro.h>
+#include "allegro5/allegro_image.h"
+#include "Entidad.h"
+
+using namespace std;
+
+class Obstaculos : public Entidad
+{
+    public:
+<<<<<<< HEAD
+//        int codigo;
+=======
+        int codigo;
+>>>>>>> 4c9fdf2c9629dd948801363e52827bbe6412a194
+        double velo;
+        Obstaculos();
+        virtual ~Obstaculos();
+    protected:
+    private:
+};
+
+#endif // OBSTACULOS_H
